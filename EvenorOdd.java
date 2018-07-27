67f28352c71d46f6ebb681e8faafe30828d2e4e9
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-07-27 11:30:40 +0530

    Create EvenorOdd.java

---
+++ EvenorOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Simpleprogram
+{
+	public static void main(String args[])
+	{
+		if(n%2==0)
+		{
+			System.out.println("Even");
+		}
+		else{
+			System.out.println("Odd");
+		    }
+	}
+}
