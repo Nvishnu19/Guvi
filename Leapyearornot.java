c8a72ecbc33b4f2106bca8f343bbc71f3af93337
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-07-27 11:39:10 +0530

    Create Leapyearornot.java

---
+++ Leapyearornot.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Simpleprogram1
+{
+	public static void main(String args[])
+	{
+		if(n%4==0)
+		{
+			System.out.println("leapyear");
+		}
+		else{
+			System.out.println("notleapyear");
+		    }
+	}
+}
