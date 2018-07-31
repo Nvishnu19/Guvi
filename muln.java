b874068dc0fa1c957e3deaf875bc011eabf328d8
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-07-31 10:42:37 +0530

    Update and rename factorial.java to muln.java

--- muln.java
+++ muln.java
@@ -5,23 +5,17 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class muln
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	Scanner sc=new Scanner(System.in);
-	int n=sc.nextInt();
-	int fact=1;
-	if(n<0)
-	System.out.println("Error");
-	else
-	{
-		for(int i=1;i<=n;++i)
-	{
-	fact=fact*i;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int d=5;
+		for(int i=1;i<=d;i++)
+		{
+				int mul=n*i;
+				System.out.print(mul+" ");
+		} 
 	}
-	System.out.println("factorial of number:"+fact);
-
-	}
-}
 }
