b2a8b239e7e33875c5271c1ea91b30707c8d6960
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-07-31 12:01:55 +0530

    Update Sumofnaturalno.java

--- Sumofnaturalno.java
+++ Sumofnaturalno.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Sumofnaturalno
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -13,7 +13,7 @@ class Ideone
 	int n=sc.nextInt();
 	int sum=0;
 	int i;
-	for(i=1;i<=n;++i)
+	for(i=1;i<=n;i++)
 	{
 		sum=sum+i;
 	}
