0bed9b79fb02848db7fccb72a44db13321b8a380
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-07-31 11:21:22 +0530

    Update EvenorOdd.java

--- EvenorOdd.java
+++ EvenorOdd.java
@@ -1,16 +1,29 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class Simpleprogram
+
+/* Name of the class has to be "Main" only if the class is public. */
+class EvenorOdd
 {
-	public static void main(String args[])
+	public static void main (String[] args) throws java.lang.Exception
 	{
-		if(n%2==0)
+		// your code goes here
+		Scanner  sc=new Scanner(System.in)
+		int n=sc.nextInt();
+		if(n>0)
 		{
-			System.out.println("Even");
-		}
-		else{
-			System.out.println("Odd");
+			if(n%2==0)
+			{
+				System.out.println("Even");
+			}
+			else
+			{
+				System.out.println("Odd");
 		    }
+		}
+		else
+		{
+			System.out.println("invalid");
+		}
 	}
 }
