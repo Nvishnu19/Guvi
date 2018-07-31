45989a0b75a8406e1cae36d532541d488a661368
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-07-31 12:18:18 +0530

    Update powerofnumber.java

--- powerofnumber.java
+++ powerofnumber.java
@@ -5,14 +5,14 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class powerofnumber
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int base=sc.nextInt();
 		int exponent=sc.nextInt();
-		double result=Math.pow(base,exponent);
-		System.out.println("answer  = "+result);
+		int result=Math.pow(base,exponent);
+		System.out.println(result);
 	}
 }
