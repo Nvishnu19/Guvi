61df835540d76d8e1f9ebb95ff38077e9c292802
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-08-03 14:32:24 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Palindrome
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=0, b, i;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		i=n;
+    while( n!=0&&n<=1000)
+	{
+    	b = n%10;
+        a = a*10 + b;
+        n = n/10;
+    }
+    if (i==a)
+        System.out.println(" yes");
+    else
+        System.out.println("no");
+    
+	}
+}
