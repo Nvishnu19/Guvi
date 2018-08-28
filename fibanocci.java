5d7c3fa62f6752e83bc86fc9db7ce53bf8129f07
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-08-28 09:26:46 +0530

    Create fibanocci.java

---
+++ fibanocci.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int  t1 = 1, t2 = 1;
+		int n=sc.nextInt();
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }	
+}
