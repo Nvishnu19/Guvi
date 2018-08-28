af375ae6824b53ecbbdbafe78e461b0bf7f10a52
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-08-28 10:18:44 +0530

    Create range2.java

---
+++ range2.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+ 		if(i>=n1&&i<=n2)
+					System.out.println("yes");    
+		else
+					System.out.println("no");
+				}
+}
