57b955ef57472d530277db7470ff82f530dd3e86
Nvishnu19 <41769746+Nvishnu19@users.noreply.github.com>
2018-09-04 15:37:36 +0530

    Update and rename primeinterval.java to primeinterval1.java

--- primeinterval1.java
+++ primeinterval1.java
@@ -9,7 +9,7 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc=new Scanner(System.in);
+Scanner sc=new Scanner(System.in);
 	int i =0,num=0;
 		int n1=sc.nextInt();
 		int n2=sc.nextInt();
@@ -17,18 +17,27 @@ class Ideone
 		String  primeNumbers = "";
 		for (i=n1+1; i <n2; i++)        
 		{ 		  	  
-			int counter=0; 	  
+			int counter=0,p=0;	  
 			for(num =i; num>=1; num--)
 			{
             			if(i%num==0)
 				{
  					counter = counter + 1;
+ 					p++;
 				}
 			 }
 			if (counter ==2)
 			{
-				//Appended the Prime number to the String
-				primeNumbers = primeNumbers + i + " ";
+				if(p==1)
+				{
+					System.out.print(i);
+				}
+				else
+				{
+					System.out.print(i+" ");
+					p--;
+				}
+				//Appended the Prime number to the String			}
 			}
 		}
 				
